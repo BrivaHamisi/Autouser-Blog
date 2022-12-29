78da77d42030869960852dbd4f91ceeed0fb1485
BrivaHamisi <brivahamisi@gmail.com>
2022-12-29 16:17:31 +0300

    Latest Update

--- src/App.js
+++ src/App.js
@@ -3,11 +3,16 @@ import './App.css';
 function App() {
 
   const title = 'Welcome to the New Blog'
+  const Likes = 50
+  const link = 'https://www.google.com/'
 
   return (
     <div className="App">
       <div className="content">
         <h1>{title}</h1>
+        <p>Liked {Likes} times</p>
+
+        <a href={link}>Google Site</a>
       </div>
     </div>
   );
