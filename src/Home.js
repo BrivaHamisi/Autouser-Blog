876f7ee0cf76a8bca1d5e7c9e9e8ee3de9f28287
BrivaHamisi <brivahamisi@gmail.com>
2023-01-02 18:00:21 +0300

    Functions

--- src/Home.js
+++ src/Home.js
@@ -1,7 +1,12 @@
 const Home = () => {
+
+    const handleClick = () =>{
+        console.log("Hello, Autousers")
+    }
     return ( 
         <div className="home">
             <h2>HomePage</h2>
+            <button onClick={handleClick}>Click Me</button>
         </div>
      );
 }
