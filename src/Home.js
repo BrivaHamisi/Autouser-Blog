c48e948d7fdc23001bca666f35e77c83784e814e
BrivaHamisi <brivahamisi@gmail.com>
2023-01-11 11:10:44 +0300

    Click Events

--- src/Home.js
+++ src/Home.js
@@ -1,12 +1,19 @@
 const Home = () => {
 
-    const handleClick = () =>{
-        console.log("Hello, Autousers")
+    const handleClick = (e) =>{
+        console.log("Hello, Autousers",e)
     }
+
+    const handleClickAgain = (name, e) =>{
+        console.log("Hello " + name, e.target)
+    }
+
+
     return ( 
         <div className="home">
             <h2>HomePage</h2>
             <button onClick={handleClick}>Click Me</button>
+            <button onClick={(e)=> handleClickAgain('Hamisi', e)}>Click Me Again</button>
         </div>
      );
 }
