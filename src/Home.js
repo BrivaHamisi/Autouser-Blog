32189c60598397e25b526cf3ac777a1f6596e807
BrivaHamisi <brivahamisi@gmail.com>
2023-01-11 13:19:53 +0300

    Outputting a list

--- src/Home.js
+++ src/Home.js
@@ -1,23 +1,21 @@
 import { useState } from "react";
 
 const Home = () => {
-    // let name = 'Hamisi';
-    const [name, setName] = useState('Hamisi');
-    const [age, setAge] = useState(25);
-
-    const handleClick = () =>{
-        setName('Briva');
-        setAge(23);
-    }
-
-
-    return ( 
+    const [blogs, setBlogs] = useState([
+        { title: 'My new website', body: 'lorem ipsum...', author: 'Hamisi Esq', id: 1 },
+        { title: 'Welcome party!', body: 'lorem ipsum...', author: 'Briva', id: 2 },
+        { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'Hamisi Esq', id: 3 }
+    ])
+    return (
         <div className="home">
-            <h2>HomePage</h2>
-            <p>{name} is {age} years old</p>
-            <button onClick={handleClick}>Click Me</button>
+            {blogs.map((blog) => (
+                <div className="blog-preview" key={blog.id}>
+                    <h2>{blog.title}</h2>
+                    <p>Written by {blog.author}</p>
+                </div>
+            ))}
         </div>
-     );
+    );
 }
- 
+
 export default Home;
