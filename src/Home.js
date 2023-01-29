b7c8bfcd1349496d7855d44a47394359f0e3766d
BrivaHamisi <brivahamisi@gmail.com>
2023-01-29 22:16:18 +0300

    Conditional Loading Message

--- src/Home.js
+++ src/Home.js
@@ -4,20 +4,29 @@ import BlogList from "./BlogList";
 const Home = () => {
     const [blogs, setBlogs] = useState(null)
 
-    const handleDelete =(id) =>{
-        const newBlogs = blogs.filter(blogs => blogs.id !== id)
-        setBlogs(newBlogs);
-    }
+    const [isPending, setIsPending] = useState(true)
+
+    // const handleDelete =(id) =>{
+    //     const newBlogs = blogs.filter(blogs => blogs.id !== id)
+    //     setBlogs(newBlogs);
+    // }
     useEffect(()=>{
         fetch('http://localhost:8000/blogs')
         .then(res =>{
-            res.json
-        })
+            return res.json()
+         })
+         .then(data => {
+            console.log(data);
+            setBlogs(data);
+            setIsPending(false)
+
+         })
     }, [])
 
     return (
         <div className="home">
-            <BlogList blogs={blogs} title="All Blogs!" handleDelete={handleDelete}/>
+            {isPending && <div>Loading...</div>}
+            { blogs && <BlogList blogs={blogs} title="All Blogs!"/>}
         </div>
     );
 }
